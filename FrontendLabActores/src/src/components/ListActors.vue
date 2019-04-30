6c3dfe26ca446392cc8ab4b5d7187662c9503ffe
Vicente Vega <vicente.vega@usach.cl>
2019-04-29 23:45:05 -0400

    agregado front

---
+++ FrontendLabActores/src/src/components/ListActors.vue
@@ -0,0 +1,26 @@
+<template>
+	<div class="actors">
+		<li v-for="actor in actors">{{ actor.firstName }}</li>
+	</div>
+</template>
+
+<script>
+	import axios from 'axios'
+
+	export default {
+		name: 'ListActors',
+		data() {
+			return {
+				actors: []
+			};
+		},
+		created(){
+			axios.get('http://localhost:8081/actors').
+			then( response => {
+				this.actors = response.data;
+				});
+		}
+	}
+
+</script>
+
---
+++ FrontendLabActores/src/src/views/Actors.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="container">
+    <ListActors/>
+    </div>
+</template>
+
+<script>
+import ListActors from '@/components/ListActors.vue'
+export default {
+    name: 'Actors',
+    components: {
+        ListActors
+    }
+}
+</script>
