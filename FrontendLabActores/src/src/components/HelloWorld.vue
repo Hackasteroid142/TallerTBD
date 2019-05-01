30e4264c978f0963f2394f6925e8dc5e53e5ea8b
Vicente Vega <vicente.vega@usach.cl>
2019-05-01 18:44:25 -0400

    cambio de colores

--- FrontendLabActores/src/src/components/HelloWorld.vue
+++ FrontendLabActores/src/src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <header class="masthead">
     <div class="container">
       <div class="intro-text">
-        <div class="intro-lead-in" > Taller 1 TBD</div>
+        <div class="intro-lead-in"> Taller 1 TBD</div>
         <div class="intro-heading text-uppercase"></div>
         <v-btn color="pink white--text" href="https://github.com/Hackasteroid142/TallerTBD">Github</v-btn>
         <v-btn color="pink white--text" href="#/actors">Actores</v-btn>
